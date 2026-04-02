c7b22da93bc58866127eb988c3c46435147adbd9
bidingurusmk <bidingurusmk@gmail.com>
2026-04-02 12:01:44 +0700

    rev

--- app/admin/customers/add/page.tsx
+++ app/admin/customers/add/page.tsx
@@ -1,6 +1,17 @@
+import { getCookie } from "@/helper/cookies";
 import AddCustomerForm from "./form";
 
-export default function AdminCustomerPage() {
+export default async function AdminCustomerPage() {
+  const services = await 
+  fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/services?page=1&quantity=1000`,{
+    method: "GET",
+    headers: {
+      "APP-KEY": process.env.NEXT_PUBLIC_APP_KEY || "",
+      authorization: `Bearer ${await getCookie("token")}`,
+    },
+  });
+  const servicesData = await services.json();
+  console.log(servicesData);
   return (
     <div className="w-full p-5">
       <h1 className="text-2xl font-bold">
